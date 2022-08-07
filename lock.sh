f1fb9a3ee90b0c79934416c4bc996852f2961221
sioodmy <sioodmy@tuta.io>
2022-08-07 19:24:01 +0200

    init: lock script

---
+++ lock.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+swaylock \
+    --color 040c16 \
+	--clock \
+	--indicator \
+	--indicator-radius 200 \
+	--indicator-thickness 20 \
+	--ring-color 17252A \
+	--key-hl-color 92bbed \
+    --text-color cce9ea \
+	--line-color 040c16 \
+	--inside-color 040c16\
+	--separator-color 00000000 \
+    --text-caps-lock-color e2d06a \
+    --line-ver-color 040c16 \
+    --ring-ver-color 17252A \
+    --inside-ver-color 040c16 \
+    --text-ver-color cce9ea \
+    --ring-wrong-color E6676B \
+    --text-wrong-color cce9ea \
+    --inside-wrong-color 040c16 \
+    --inside-clear-color 040c16 \
+    --text-clear-color cce9ea \
+    --ring-clear-color A2E4B8 \
+    --bs-hl-color e6676b \
+    --line-uses-ring \
+    --grace 2 \
+    --datestr "%d.%m" \
+	--fade-in 0.1
+
